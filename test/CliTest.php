cdbd1ef5d8edc23a1638c99ce884a87f9d9e99c0
Wojciech Brozyna <wojciech.brozyna@gmail.com>
2015-12-17 12:26:55 +0000

    PHP Unit implementation

---
+++ test/CliTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace test;
+
+use mcommander\Cli;
+
+class TestCli extends \PHPUnit_Framework_TestCase {
+    
+    public function testTest() 
+    {
+        
+        $foo = true;
+        $this->assertTrue($foo);
+        
+    }
+    
+}
